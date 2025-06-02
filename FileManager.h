c9d948b862cc77235228095903effab8b4bc2fe1
TejaDirba <teja.tupinyte@gmail.com>
2025-06-02 15:52:31 +0300

    Update  FileManager.h

---
+++ FileManager.h
@@ -0,0 +1,25 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include "Student.h"
+#include <vector>
+#include <string>
+
+class FileManager {
+public:
+    static std::vector<Student> readStudentsFromFile(const std::string& filename);
+    static void writeResultsToFile(const std::vector<Student>& students, 
+                                   const std::string& filename = "results.txt");
+    static void printResults(const std::vector<Student>& students);
+    static void printHeader();
+
+    static void splitAndWrite(const std::vector<Student>& all,
+                              const std::string& baseFilename);
+    static void generateTestFiles();
+
+private:
+    static void parseStudentLine(const std::string& line, Student& student);
+    static bool isValidGrade(int grade);
+};
+
+#endif // FILEMANAGER_H
